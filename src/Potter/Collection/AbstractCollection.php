c808f76afb6da8af825b8a4b4783708a8dc1ab86
Jay Potter <j@ypotter.ca>
2024-11-02 20:38:24 -0400

    Create AbstractCollection.php

---
+++ src/Potter/Collection/AbstractCollection.php
@@ -0,0 +1,10 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Potter\Collection;
+
+use Illuminate\Support\Collection as BaseCollection;
+
+abstract class AbstractCollection extends BaseCollection implements CollectionInterface
+{ }
